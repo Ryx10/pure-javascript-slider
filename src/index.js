ed62ab5d11f3fcbeb54749410f09e26932f7cc12
Mateusz <mat.grzybowski@gmail.com>
2016-12-24 21:11:47 +0100

    add navigation

--- src/index.js
+++ src/index.js
@@ -16,13 +16,48 @@ simpleSlider.prototype.slidesStyle = function(){
         slide.style.float = "left";
         slide.children[0].style.width = '100%' ;
     }
+};
+simpleSlider.prototype.slideRight = function(){
+    this.counter += 100 / this.slides.length;
+    this.counter = this.counter >=100 ? 0 : this.counter;
+    this.slider.style.transform = "translateX(-" + this.counter + "%)";
+};
+simpleSlider.prototype.slideLeft = function(){
+    this.counter -= 100 / this.slides.length;
+    this.counter = this.counter < 0 ? 100 - 100 / this.slides.length : this.counter;
+    this.slider.style.transform = "translateX(-" + this.counter + "%)";
+};
+simpleSlider.prototype.sliderFunc = null;
+simpleSlider.prototype.intervalSlide = function(){
+    this.sliderFunc = setInterval(this.slideRight.bind(this), 3000);
 }
-simpleSlider.prototype.slideAnimation = function(){
-        this.counter += 100 / this.slides.length;
-        this.counter = this.counter >=100 ? 0 : this.counter;
-        this.slider.style.transform = "translateX(-" + this.counter + "%)";
+simpleSlider.prototype.stopInterval = function(){
+    window.clearInterval(this.sliderFunc);
+};
+simpleSlider.prototype.navigation = function(){
+    var left = document.createElement('p');
+    var right = document.createElement('p');
+    left.className = 'simpleLeft';
+    right.className = 'simpleRight';
+    left.style.cssText = 'position: absolute; width: 25px; height:25px; left:10px; top:50%; border-left:2px solid black; border-bottom:2px solid black;transform:translateY(-50%) rotate(45deg);cursor:pointer;';
+    right.style.cssText = 'position: absolute; width: 25px; height:25px; right:10px; top:50%; border-right:2px solid black; border-bottom:2px solid black;transform:translateY(-50%) rotate(-45deg);cursor:pointer;';
+    this.slider.parentNode.append(left, right);
+    left.addEventListener('click', function(){
+        this.slideLeft();
+        this.stopInterval();
+        this.intervalSlide()
+    }.bind(this));
+    right.addEventListener('click', function(){
+        this.slideRight();
+        this.stopInterval();
+        this.intervalSlide()
+    }.bind(this))
+
+
 };
 simpleSlider.prototype.init = function () {
     this.slidesStyle();
-    setInterval(this.slideAnimation.bind(this), 3000);
+    this.intervalSlide();
+    this.navigation();
+
 };
