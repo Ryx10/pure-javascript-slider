d305c5a95fa314c0f5554c4c220d30624f7314bf
Mateusz <mat.grzybowski@gmail.com>
2016-12-29 18:42:22 +0100

    commit

--- src/index.js
+++ src/index.js
@@ -1,13 +1,16 @@
 var simpleSlider = function(options){
     this.slider = document.querySelector(options.selector);
     this.slides = document.querySelectorAll(options.selector + ' li');
+    this.interval = options.interval ? options.interval : 3000;
+    this.animationSpeed = options.animationSpeed ? options.animationSpeed : 0.3;
+    this.navigationColor = options.navigationColor ? options.navigationColor : '#000000';
     this.counter = 0;
 };
 simpleSlider.prototype.slidesStyle = function(){
     var vievportWidth = this.slider.parentNode.offsetWidth * this.slides.length;
     var slideWidth = vievportWidth / this.slides.length;
     this.slider.style.width = vievportWidth + 'px';
-    this.slider.style.transition = 'transform 0.3s ease';
+    this.slider.style.transition = 'transform ' + this.animationSpeed + 's ease';
     this.slider.parentNode.style.overflow = 'hidden';
     this.slider.parentNode.style.position = 'relative';
     for(var slide of this.slides){
@@ -29,7 +32,7 @@ simpleSlider.prototype.slideLeft = function(){
 };
 simpleSlider.prototype.sliderFunc = null;
 simpleSlider.prototype.intervalSlide = function(){
-    this.sliderFunc = setInterval(this.slideRight.bind(this), 3000);
+    this.sliderFunc = setInterval(this.slideRight.bind(this), this.interval);
 };
 simpleSlider.prototype.stopInterval = function(){
     window.clearInterval(this.sliderFunc);
@@ -39,8 +42,8 @@ simpleSlider.prototype.navigation = function(){
     var right = document.createElement('p');
     left.className = 'simpleLeft';
     right.className = 'simpleRight';
-    left.style.cssText = 'position: absolute; width: 25px; height:25px; left:10px; top:50%; border-left:2px solid black; border-bottom:2px solid black;transform:translateY(-50%) rotate(45deg);cursor:pointer;';
-    right.style.cssText = 'position: absolute; width: 25px; height:25px; right:10px; top:50%; border-right:2px solid black; border-bottom:2px solid black;transform:translateY(-50%) rotate(-45deg);cursor:pointer;';
+    left.style.cssText = 'position: absolute; width: 25px; height:25px; left:15px; top:50%; border-left:2px solid' +  this.navigationColor + '; border-bottom:2px solid' +  this.navigationColor + ';transform:translateY(-50%) rotate(45deg);cursor:pointer;';
+    right.style.cssText = 'position: absolute; width: 25px; height:25px; right:15px; top:50%; border-right:2px solid' +  this.navigationColor + '; border-bottom:2px solid' +  this.navigationColor + ';transform:translateY(-50%) rotate(-45deg);cursor:pointer;';
     this.slider.parentNode.append(left, right);
     left.addEventListener('click', function(){
         this.slideLeft();
@@ -56,8 +59,8 @@ simpleSlider.prototype.navigation = function(){
 
 };
 simpleSlider.prototype.init = function () {
+    this.navigation();
     this.slidesStyle();
     this.intervalSlide();
-    this.navigation();
 
 };
