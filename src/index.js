776bd54407b11f33691aa01fe3928440a6b52995
Mateusz <mat.grzybowski@gmail.com>
2016-12-29 23:33:30 +0100

    commit

--- src/index.js
+++ src/index.js
@@ -1,12 +1,24 @@
 var simpleSlider = function(options){
     this.slider = document.querySelector(options.selector);
     this.slides = document.querySelectorAll(options.selector + ' li');
+
+    this.type = options.type ? options.type : 'slide';
     this.interval = options.interval ? options.interval : 3000;
+    this.imgPadding = options.imgPadding ? options.imgPadding : '';
     this.animationSpeed = options.animationSpeed ? options.animationSpeed : 0.3;
     this.navigationColor = options.navigationColor ? options.navigationColor : '#000000';
     this.counter = 0;
 };
-simpleSlider.prototype.slidesStyle = function(){
+simpleSlider.prototype.addStyle = function(){
+    for(var slide of this.slides){
+        slide.style.cssText +=  'box-sizing: border-box;' +
+                                'padding: ' + this.imgPadding + 'px;';
+    }
+};
+
+/* Slide */
+
+simpleSlider.prototype.slideStyle = function(){
     var vievportWidth = this.slider.parentNode.offsetWidth * this.slides.length;
     var slideWidth = vievportWidth / this.slides.length;
     this.slider.style.width = vievportWidth + 'px';
@@ -17,7 +29,7 @@ simpleSlider.prototype.slidesStyle = function(){
         slide.style.width = slideWidth +'px';
         slide.style.display = "block";
         slide.style.float = "left";
-        slide.children[0].style.width = '100%' ;
+        slide.children[0].style.width = '100%';
     }
 };
 simpleSlider.prototype.slideRight = function(){
@@ -30,20 +42,29 @@ simpleSlider.prototype.slideLeft = function(){
     this.counter = this.counter < 0 ? 100 - 100 / this.slides.length : this.counter;
     this.slider.style.transform = "translateX(-" + this.counter + "%)";
 };
-simpleSlider.prototype.sliderFunc = null;
-simpleSlider.prototype.intervalSlide = function(){
-    this.sliderFunc = setInterval(this.slideRight.bind(this), this.interval);
-};
-simpleSlider.prototype.stopInterval = function(){
-    window.clearInterval(this.sliderFunc);
-};
-simpleSlider.prototype.navigation = function(){
+simpleSlider.prototype.slideNavigation = function(){
     var left = document.createElement('p');
     var right = document.createElement('p');
     left.className = 'simpleLeft';
     right.className = 'simpleRight';
-    left.style.cssText = 'position: absolute; width: 25px; height:25px; left:15px; top:50%; border-left:2px solid' +  this.navigationColor + '; border-bottom:2px solid' +  this.navigationColor + ';transform:translateY(-50%) rotate(45deg);cursor:pointer;';
-    right.style.cssText = 'position: absolute; width: 25px; height:25px; right:15px; top:50%; border-right:2px solid' +  this.navigationColor + '; border-bottom:2px solid' +  this.navigationColor + ';transform:translateY(-50%) rotate(-45deg);cursor:pointer;';
+    left.style.cssText +=   'position: absolute; ' +
+        'width: 25px; ' +
+        'height:25px; ' +
+        'left:15px; ' +
+        'top:50%; ' +
+        'border-left:2px solid' +  this.navigationColor + '; ' +
+        'border-bottom:2px solid' +  this.navigationColor + ';' +
+        'transform:translateY(-50%) rotate(45deg);' +
+        'cursor:pointer;';
+    right.style.cssText +=  'position: absolute;' +
+        ' width: 25px; ' +
+        'height:25px; ' +
+        'right:15px; ' +
+        'top:50%; ' +
+        'border-right:2px solid' +  this.navigationColor + '; ' +
+        'border-bottom:2px solid' +  this.navigationColor + ';' +
+        'transform:translateY(-50%) rotate(-45deg);' +
+        'cursor:pointer;';
     this.slider.parentNode.append(left, right);
     left.addEventListener('click', function(){
         this.slideLeft();
@@ -54,13 +75,62 @@ simpleSlider.prototype.navigation = function(){
         this.slideRight();
         this.stopInterval();
         this.intervalSlide()
-    }.bind(this))
-
-
+    }.bind(this));
 };
-simpleSlider.prototype.init = function () {
-    this.navigation();
-    this.slidesStyle();
-    this.intervalSlide();
 
+/* End slide */
+
+/* Fade in */
+
+simpleSlider.prototype.fadeInStyle = function(){
+    this.slider.style.cssText += 'position: relative; width: 100%';
+    for(var slide of this.slides){
+        slide.style.cssText +=  'position: absolute; ' +
+                                'width: 100%; ' +
+                                'opacity:0;' +
+                                'transition: opacity ' + this.animationSpeed + 's ease';
+        //If img are linked somewhere
+        if(slide.querySelector('a')) slide.querySelector('a').style.cssText += 'display: block; position: relative; z-index:1';
+
+        slide.querySelector('img').style.width = '100%';
+    }
+    this.slider.style.height = this.slides[0].querySelector('img').offsetHeight + 'px';
+};
+simpleSlider.prototype.rightFadeIn = function(){
+    var current = this.counter === this.slides.length ? 0 : this.counter;
+    var previous = current === 0 ? this.slides.length - 1 : current - 1;
+    this.slides[previous].style.opacity = 0;
+    this.slides[current].style.opacity = 1;
+    this.counter = this.counter === this.slides.length ? 0 : this.counter;
+    this.counter++;
+};
+
+/* End fade in */
+
+/* Interval for different slider styles */
+simpleSlider.prototype.sliderFunc = null;
+simpleSlider.prototype.intervalSlide = function(){
+    this.sliderFunc = setInterval(this.slideRight.bind(this), this.interval);
+};
+simpleSlider.prototype.intervalFadeIn = function(){
+    this.rightFadeIn();
+    this.sliderFunc = setInterval(this.rightFadeIn.bind(this), this.interval);
+};
+simpleSlider.prototype.stopInterval = function(){
+    window.clearInterval(this.sliderFunc);
+};
+
+/* End interval */
+
+simpleSlider.prototype.init = function () {
+    this.addStyle();
+
+    if(this.type === 'fadeIn'){
+        this.fadeInStyle();
+        this.intervalFadeIn();
+        this.slideNavigation();
+    } else {
+        this.slideStyle();
+        this.intervalSlide();
+    }
 };
