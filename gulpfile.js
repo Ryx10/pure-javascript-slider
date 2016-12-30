2ead67d7b2dd942350048f08bd22054fd55f1020
Mateusz <mat.grzybowski@gmail.com>
2016-12-30 20:53:48 +0100

    gulpfile update

--- gulpfile.js
+++ gulpfile.js
@@ -1,7 +1,8 @@
 var gulp = require('gulp'),
     gp_concat = require('gulp-concat'),
     gp_rename = require('gulp-rename'),
-    gp_uglify = require('gulp-uglify');
+    gp_uglify = require('gulp-uglify'),
+    gp_clean =  require('gulp-clean');
 
 gulp.task('js-fef', function(){
     return gulp.src('src/*.js')
@@ -10,4 +11,9 @@ gulp.task('js-fef', function(){
         .pipe(gp_rename('index.js'))
         .pipe(gp_uglify())
         .pipe(gulp.dest('.'));
+});
+
+gulp.task('clean', ['js-fef'], function(){
+    return gulp.src('concat.js')
+        .pipe(gp_clean({force: true}));
 });
